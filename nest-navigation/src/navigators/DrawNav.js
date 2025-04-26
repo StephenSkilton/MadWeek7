6e9fb14fc017af2f4e0e46e8e97a041513118d66
StephenSkilton <stephen.skilton@gmail.com>
2025-04-27 02:10:06 +1000

    Add files via upload

---
+++ nest-navigation/src/navigators/DrawNav.js
@@ -0,0 +1,22 @@
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import Home from "../screens/Home";
+
+// import ProdList from "../screens/ProdList";
+
+import StackNav from "./StackNav";
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawNav() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Products" component={StackNav} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ nest-navigation/src/navigators/StackNav.js
@@ -0,0 +1,14 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import ProdList from "../screens/ProdList";
+// import ProdDetail from "../screens/ProdDetail";
+import TabNav from "./TabNav";
+const Stack = createStackNavigator();
+
+export default function StackNav() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="ProdList" component={ProdList} />
+      <Stack.Screen name="ProdDetail" component={TabNav} />
+    </Stack.Navigator>
+  );
+}
